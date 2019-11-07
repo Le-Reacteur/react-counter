388caab1d203d081698e602532e440d4345cd02b
Farid Safi <FaridSafi@users.noreply.github.com>
2019-11-07 10:02:00 +0100

    step 1 : done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+// 1. Faire la mise en page (html / css)
+// 2. Créer les états
+// 3. Mettre en place les interactions (onClick, etc.)
+
 function App() {
-  return <div></div>;
+  // Définir une valeur par défaut
+  // Créer la variable qui va représenter l'état
+  // Créer la fonction qui va permettre de modifier l'état
+  const [counter1, setCounter1] = useState(0);
+
+  // HTML + JS = JSX
+  return (
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column"
+      }}
+    >
+      <p>{counter1}</p>
+      <div>
+        <button
+          onClick={() => {
+            setCounter1(counter1 - 1);
+            // Ne jamais modifier directement une variable d'état :
+            // counter1 = counter1 - 1;
+            // counter1++;
+          }}
+        >
+          -
+        </button>
+        <button
+          onClick={() => {
+            setCounter1(counter1 + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
