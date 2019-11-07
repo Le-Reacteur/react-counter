c9542320a5822909fea2fdb3652e48dc1286259c
Farid Safi <FaridSafi@users.noreply.github.com>
2019-11-07 10:31:04 +0100

    step 3 : done

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,5 +1,19 @@
 import React from "react";
 
+const renderDecrement = props => {
+  if (props.value > 0) {
+    return <button onClick={props.onDecrement}>-</button>;
+  }
+  return null;
+};
+
+const renderIncrement = props => {
+  if (props.value < 10) {
+    return <button onClick={props.onIncrement}>+</button>;
+  }
+  return null;
+};
+
 const Counter = props => {
   console.log(props.value); // 0
   console.log(props.onIncrement); // function
@@ -9,11 +23,37 @@ const Counter = props => {
     <>
       <p>{props.value}</p>
       <div>
-        <button onClick={props.onDecrement}>-</button>
-        <button onClick={props.onIncrement}>+</button>
+        {renderDecrement(props)}
+        {renderIncrement(props)}
       </div>
     </>
   );
 };
+
+// if inline :
+// return (
+//   <>
+//     <p>{props.value}</p>
+//     <div>
+//       {props.value > 0 && <button onClick={props.onDecrement}>-</button>}
+//       {props.value < 10 && <button onClick={props.onIncrement}>+</button>}
+//     </div>
+//   </>
+// );
+
+// avec ternaire :
+// return (
+//   <>
+//     <p>{props.value}</p>
+//     <div>
+//       {props.value > 0 ? (
+//         <button onClick={props.onDecrement}>-</button>
+//       ) : null}
+//       {props.value < 10 ? (
+//         <button onClick={props.onIncrement}>+</button>
+//       ) : null}
+//     </div>
+//   </>
+// );
 
 export default Counter;
